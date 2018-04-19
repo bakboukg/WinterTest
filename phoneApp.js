532e0fa4db460bb0dab285d5ad4a821944e98a37
bakboukg <bakboukg@kenyon.edu>
2018-04-19 15:45:23 -0400

    Update phoneApp.js

--- phoneApp.js
+++ phoneApp.js
@@ -11,9 +11,9 @@ $(document).ready(function () {
 	console.log("hidden");
     operation = "SearchAll";
 
-    $(".SearchOptions li a").click(function(){
+    $('#SearchOptions li a').click(function(){
 	console.log("pick!"+$(this).text());
-	$(this).parents(".btn-group").find('.selection').text($(this).text());*/
+	//$(this).parents(".btn-group").find('.selection').text($(this).text());
 	operation=$(this).text();
 	changeOperation(operation);
     });
